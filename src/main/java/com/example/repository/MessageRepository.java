c9bdd46857de476d41fae4772ff2478e673c72f0
Ariesh Grover <arieshgrover0@gmail.com>
2023-09-26 18:39:12 +0000

    finished account service

--- src/main/java/com/example/repository/MessageRepository.java
+++ src/main/java/com/example/repository/MessageRepository.java
@@ -1,4 +1,14 @@
 package com.example.repository;
 
-public interface MessageRepository {
+import com.example.entity.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+    
+    public Message findMessageByID(int id);
+
+    public List<Message> findAllMessagesByPosted_By(int posted_by);
 }
--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,44 @@
 package com.example.service;
 
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AccountService {
+    
+    AccountRepository accountRepository;
+    @Autowired
+    public AccountService(AccountRepository accountRepository){
+        this.accountRepository = accountRepository;
+    }
+
+    /*
+     * Register a new account
+     * @param Account acc
+     * check if password is > 4 chars long
+     * check if username is not null
+     * check  if username is not already registered
+     * return account with id
+     */
+    
+     public Account register(Account acc){
+        if(acc.getPassword().length() >4 && 
+        acc.getUsername().length()> 0 && 
+        accountRepository.findByUsername(acc.getUsername()) == null) 
+            return accountRepository.save(acc);
+        else return null;
+     }
+
+
+     public Account login(Account acc){
+        Account b = accountRepository.findByUsername(acc.getUsername());
+        if(b != null && acc.equals(b)) return b;
+        else return null;
+     }
+
 }
